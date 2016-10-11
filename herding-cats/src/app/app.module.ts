38994e2f57065817b3c2b0e103651afb5417f863
tpadley <tylerp@dev6.com>
2016-10-11 16:49:25 -0400

    added split modules for lazy loading

--- herding-cats/src/app/app.module.ts
+++ herding-cats/src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {CatModule} from "./cats/cat.module";
 import {appRouting} from "./app.routes";
+import {DogModule} from "./dogs/dog.module";
 
 @NgModule({
-	imports: [CatModule, appRouting],
+	imports: [CatModule, DogModule, appRouting],
 	declarations: [AppComponent],
 	bootstrap: [AppComponent]
 })
